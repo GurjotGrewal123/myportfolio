c2e5f4139c558977bea1416685b2f64f3514ac9f
GurjotGrewal123 <gurjot_grewal@outlook.com>
2022-10-14 23:36:15 -0400

    added page router

--- src/App.js
+++ src/App.js
@@ -1,9 +1,26 @@
+import { Route, Switch } from "react-router"
+import { ThemeProvider } from "styled-components"
+import { lightTheme } from "./components/Themes"
+
 import GlobaStyle from "./globalStyles"
+import Intro from "./components/Intro"
+import About from "./components/About"
+import Work from "./components/Work"
+import Projects from "./components/Projects"
+
 
 function App() {
   return <>
     <GlobaStyle />
-    App File
+
+    <ThemeProvider theme={lightTheme}>
+      <Switch>
+        <Route exact path="/" component={Intro} />
+        <Route exact path="/about" component={About} />
+        <Route exact path="/work" component={Work} />
+        <Route exact path="/projects" component={Projects} />
+      </Switch>
+    </ThemeProvider>
   </>
 
 }
---
+++ src/components/Work.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Work = () => {
+    return (
+        <div>
+            Work Exp.
+        </div>
+    )
+}
+
+export default Work; 
