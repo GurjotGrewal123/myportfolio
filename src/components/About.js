df1fc75f5d02f371af36ed6a833fa9ee61093675
GurjotGrewal123 <gurjot_grewal@outlook.com>
2022-10-15 17:30:08 -0400

    added about me content

--- src/components/About.js
+++ src/components/About.js
@@ -11,14 +11,74 @@ position: relative;
 overflow: hidden;
 `
 
+const Computer = styled.div`
+position: absolute;
+right: 10%;
+top: 35%;
+scale: 1.5;
+`
 
+const AboutContent = styled.div`
+color: ${(props) => props.theme.text};
+border: 1px solid ${(props) => props.theme.text};
+padding: 2rem;
+width: 50vw;
+height: 60vh;
+z-index: 3;
+line-height: 1.5;
+display: flex;
+justify-content: center;
+align-items: center;
+font-size: 1rem;
+font-weight: 300;
+backdrop-filter: blur(4px);
+
+position: absolute;
+left: 10%;
+top: 10rem;
+font-family: "NTR", sans-serif;
+
+.technologies{
+    transform: translate(50px, 10px);
+}
+
+`
 
 const About = () => {
     return (
         <ThemeProvider theme={darkTheme}>
             <AboutContainer id="about">
                 <Particle theme='dark' />
-                About Me
+
+                <Computer>
+                    <iframe title="computerImg" src="https://giphy.com/embed/gH85KtY9fX2yd9eG4F" width="100%" height="100%" frameBorder="0" class="giphy-embed" allowFullScreen></iframe>
+                </Computer>
+
+                <AboutContent>
+                    <div>
+                        I am currently a second year Software Engineering major at the University of Ottawa.
+                        I have currently completed one of my five co-op terms.
+                        I love learning new technologies and am always trying to grow my skillset.
+                        I have been interested in designing programs since high-school; hence, my pursuit in this field.
+
+                        <br></br>
+                        <br></br>
+                        Here are some technologies I have been working with:
+                        <div className="technologies">
+                            <ul>
+                                <li>Javascript ES6+</li>
+                                <li>Java</li>
+                                <li>Python</li>
+                                <li>React.js</li>
+                                <li>HTML & CSS</li>
+
+                            </ul>
+                        </div>
+                        <br></br>
+                        Other than software development, I love to play basketball, video games, and edit videos.
+                    </div>
+                </AboutContent>
+
             </AboutContainer>
         </ThemeProvider>
     )
